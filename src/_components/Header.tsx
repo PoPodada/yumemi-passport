c5f68d1da156bbc18464c3ff3e564aad4a695fee
PoPodada <goito.722@icloud.com>
2024-08-30 01:24:18 +0900

    ヘッダーのUIの作成

---
+++ src/_components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="h-[100px] flex items-center px-[4%] bg-blue-400 font-bold text-2xl">
+      都道府県別の総人口推移
+    </header>
+  )
+}
+
+export default Header
